e10a64d172142e4d52eb14583de0c08dc1a903c5
KubaTaba1uga <jakub@buczynski.net.pl>
2021-11-27 19:57:16 +0100

    add mailbox guest delete view

--- shared_code/queries.py
+++ shared_code/queries.py
@@ -14,7 +14,7 @@ def get_user_owner_mailboxes_query(user):
 
 def get_user_owner_mailboxes(user):
     """
-    Returns owned mailboxes of a user.
+    Return owned mailboxes of a user.
     """
     return (owned_mailbox for owned_mailbox in get_user_owner_mailboxes_query(user))
 
@@ -25,10 +25,11 @@ def get_mailbox_query(mailbox_id):
 
 def get_mailbox_owner(mailbox_id):
     """
-    Returns owner of a mailbox or None if mailbox does not exist
+    Return owner of a mailbox or None if mailbox does not exist
     """
-    if get_mailbox_query(mailbox_id):
-        return get_mailbox_query(mailbox_id).owner
+    mailbox = get_mailbox_query(mailbox_id)
+    if mailbox:
+        return mailbox.owner
 
 
 def get_mailbox_guests_query(mailbox_id):
@@ -36,8 +37,21 @@ def get_mailbox_guests_query(mailbox_id):
 
 
 def get_mailbox_guests(mailbox_id):
+    """ Return all guests of mailbox with provided id
+    """
     return (guest_mailbox.guest for guest_mailbox in get_mailbox_guests_query(mailbox_id))
 
 
 def get_guest_mailbox(mailbox_id):
+    """
+    Return all guest mailboxes of mailbox with provided id
+    """
     return (guest_mailbox for guest_mailbox in get_mailbox_guests_query(mailbox_id))
+
+
+def get_guest(guest_id):
+    """
+    Return all guest mailboxes of mailbox with provided id
+    """
+
+    return MailboxGuestModel.objects.filter(pk=guest_id).first()
